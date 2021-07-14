3ada96f0761616953910215822b6af7c772ca85f
jpclipffel <jean-philippe.clipffel@post.lu>
2021-07-14 07:17:26 +0000

    Support for PUT method

--- plugins/httpapi/device42.py
+++ plugins/httpapi/device42.py
@@ -47,7 +47,7 @@ class HttpApi(HttpApiBase):
                 params = '&'.join([f'{k}={v}' for k, v in data.items()])
                 path = f'{path}?{params}'
                 data = json.dumps(data)
-            elif method in ['POST',]:
+            elif method in ['POST', 'PUT']:
                 data = '&'.join([f'{k}={v}' for k, v in data.items()])
             # Forge and send request
             response, response_data = self.connection.send(
